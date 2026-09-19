eb1be19891d7b4d10e07ca03202f1ad85c8d3654
sharobiddinovjaloliddin97-sudo <sharobiddinovjaloliddin97@gmail.com>
2026-09-19 18:14:56 +0500

    Fix email settings for production deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -246,16 +246,21 @@ if DEBUG:
         "django.core.mail.backends.console.EmailBackend"
     )
 else:
-    EMAIL_BACKEND = (
-        "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_BACKEND = os.getenv(
+        "EMAIL_BACKEND",
+        "django.core.mail.backends.console.EmailBackend",
     )
 
-    EMAIL_HOST = os.environ["EMAIL_HOST"]
-    EMAIL_PORT = int(
-        os.getenv("EMAIL_PORT", "587")
-    )
-    EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-    EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+
     EMAIL_USE_TLS = (
-        os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
+            os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
+    )
+
+    DEFAULT_FROM_EMAIL = os.getenv(
+        "DEFAULT_FROM_EMAIL",
+        "no-reply@velmora.local",
     )
